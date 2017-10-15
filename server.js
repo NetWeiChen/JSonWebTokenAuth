93c4520b4de5d095b1b1be0faa9fbc19cbc294f9
NetWeiChen <net.weichen@gmail.com>
2017-10-14 20:33:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,38 @@ app.use(session({ secret: config.secret, resave: false, saveUninitialized: true 
 // use JWT auth to secure the api
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
+//try redis store but redis not support windows
+// var redisStore = require('connect-redis')(session);
+// var sess = { 
+//     secret: config.secret,
+//     store: new redisStore({
+//         url:'redis://localhost'       
+//     }), 
+//     resave: false, 
+//     saveUninitialized: true 
+//     // cookie:{
+//     //     maxAge:60000,
+//     // }
+// };
+// app.use(session(sess));
+
+//try memcached
+// cookieParser = require("cookie-parser");
+// MemcachedStore = require("connect-memcached")(session);
+// app.use(cookieParser());
+// app.use(session(
+//     {
+//         secret:config.secret,
+//         key:'test',
+//         proxy:'true',
+//         store:new MemcachedStore(
+//             {
+//                 hosts:['127.0.0.1:11211'],
+//                 secret:config.secret
+//             }
+//         )
+//     }
+// ));
 // routes
 app.use('/login', require('./controllers/login.controller'));
 app.use('/register', require('./controllers/register.controller'));
